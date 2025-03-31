3f828e1ca2736e7795ca92a2d069541f71786041
Patrick <patrick@pop-os.localdomain>
2025-03-31 12:23:50 -0700

    file browser

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -2,6 +2,18 @@ return {
   "stevearc/oil.nvim",
   ---@module 'oil'
   opts = {},
+  config = function()
+    require("oil").setup({
+      columns = { "icon" },
+      keymaps = {
+        ["<C-h>"] = false,
+        ["<C-z>"] = "actions.select_split",
+        ["<C-l>"] = false,
+        ["<C-y>"] = "actions.refresh",
+      },
+      vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory in oil" }),
+    })
+  end,
   -- Optional dependencies
   dependencies = { { "echasnovski/mini.icons", opts = {} } },
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
